d8fec0c6807963248abc21e91fcc2de5d2e6b2b9
lukeb <luke.bates808@gmail.com>
2024-09-21 01:20:27 +0200

    done

--- js/shifumi.js
+++ js/shifumi.js
@@ -4,18 +4,20 @@ let result = "";
 for (let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener('click', () => {
         const player = buttons[i].innerHTML
-        const bot = buttons[Math.floor(Math.random * buttons.length)].innerHTML
+        const bot = buttons[Math.floor(Math.random() * buttons.length)].innerHTML
+
+        if (player == bot) {
+            result = "Draw"
+        } else if (
+            (player == "Rock" && bot == "Scissors") ||
+            (player == "Scissors" && bot == "Paper") ||
+            (player == "Paper" && bot == "Rock") 
+        ) {
+            result = "Win"
+        } else {
+            result = "Lose"
+        }
+
+        document.querySelector(".result").innerHTML = `player : ${player} </br>bot : ${bot} <br/>result : ${result}`
     })
-    if (player == bot) {
-        result = "Draw"
-    } else if (
-        (player == "Rock" && bot == "Scissors") ||
-        (player == "Scissors" && bot == "Paper") ||
-        (player == "Paper" && bot == "Rock") 
-    ) {
-        result = "Win"
-    } else {
-        result = "Lose"
-    }
-    document.querySelector(".resultat").innerHTML = `Joueur : ${player} </br>Robot : ${bot} <br/>Résultat : ${result}`
 }
