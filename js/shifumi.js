00ef438915812685b62f6a0e25322282b2cc94b5
bates232 <bates232@unicaen.fr>
2024-09-20 16:51:04 +0200

    all done but not the result part

--- js/shifumi.js
+++ js/shifumi.js
@@ -1,12 +1,21 @@
 const buttons = document.querySelectorAll("button");
 let result = "";
 
-for(let i = 0; i < buttons.length; i++){
+for (let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener('click', () => {
         const player = buttons[i].innerHTML
         const bot = buttons[Math.floor(Math.random * buttons.length)].innerHTML
     })
-    if(player == bot){
+    if (player == bot) {
         result = "Draw"
+    } else if (
+        (player == "Rock" && bot == "Scissors") ||
+        (player == "Scissors" && bot == "Paper") ||
+        (player == "Paper" && bot == "Rock") 
+    ) {
+        result = "Win"
+    } else {
+        result = "Lose"
     }
+    document.querySelector(".resultat").innerHTML = `Joueur : ${player} </br>Robot : ${bot} <br/>Résultat : ${result}`
 }
